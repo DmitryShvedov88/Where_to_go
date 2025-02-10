ebaba2997e1afc8d086968997fc23542384911ef
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-10 22:35:19 +0300

    Дабавляю модели

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Place
 
-# Register your models here.
+
+
+# @admin.register(Place)
+# class PostAdmin(admin.ModelAdmin):
+#     raw_id_fields = ('likes',)
+
+admin.site.register(Place)
--- catalog/models.py
+++ catalog/models.py
@@ -1,4 +1,11 @@
 from django.db import models
 
 # Create your models here.
-print("hello!")
+class Place(models.Model):
+    title = models.CharField(max_length=200, verbose_name="Место", blank=True, null=True)
+    short_description =  models.CharField(max_length=250, verbose_name='Короткое описание', blank=True, null=True)
+    long_descriptio = models.TextField(verbose_name='Длинное описание', blank=True, null=True)
+    latitude = models.FloatField(verbose_name="Широта")
+    longitude = models.FloatField(verbose_name="Долгота")
+    def __str__(self):
+        return self.title
