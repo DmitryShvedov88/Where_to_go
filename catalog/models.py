334fa87183a95bc2b09c069c6743e9142ea23bbb
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-11 22:09:59 +0300

    Добавляю запись фото

--- catalog/models.py
+++ catalog/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class Place(models.Model):
     title = models.CharField(max_length=200, verbose_name="Место", blank=True, null=True)
-    short_description =  models.CharField(max_length=250, verbose_name='Короткое описание', blank=True, null=True)
+    short_description = models.CharField(max_length=250, verbose_name='Короткое описание', blank=True, null=True)
     long_descriptio = models.TextField(verbose_name='Длинное описание', blank=True, null=True)
     latitude = models.FloatField(verbose_name="Широта")
     longitude = models.FloatField(verbose_name="Долгота")
@@ -12,7 +12,7 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, verbose_name="Место", on_delete=models.CASCADE, related_name="images")
+    place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="images")
     image = models.ImageField(upload_to="images/")
     number_image = models.IntegerField(default=0)
     def __str__(self):
