de363216244997729e1ddb588822b58a439ec135
Шведов Дмитрий <shvedovds@kamaz.ru>
2025-02-14 15:42:47 +0300

    GeoJSON

--- catalog/views.py
+++ catalog/views.py
@@ -1,37 +1,29 @@
 from django.shortcuts import render
+from catalog.models import Place, Image
 
+# Create your views here.
 # Create your views here.
 def index(request):
     print("Hi")
-    data = {
-        "places": {
-            "type": "FeatureCollection",
-            "features": [
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [37.62, 55.793676]
-                        },
-                    "properties": {
-                        "title": "«Легенды Москвы",
-                        "placeId": "moscow_legends",
-                        "detailsUrl": "static/places/moscow_legends.json"
-                        }
-                    },
-                {
-                    "type": "Feature",
-                    "geometry": {
-                        "type": "Point",
-                        "coordinates": [37.64, 55.753676]
-                        },
-                    "properties": {
-                        "title": "Крыши24.рф",
-                        "placeId": "roofs24",
-                        "detailsUrl": "static/places/roofs24.json"
-                        },
-                    },
-                ],
-            }
+    places = Place.objects.all()
+    features = [
+        {
+            "type": "Feature",
+            "geometry": {
+                "type": "Point",
+                "coordinates": [place.latitude, place.longitude]
+                },
+            "properties": {
+                "title": place.title,
+                "placeId": place.id,
+                "detailsUrl": "static/places/moscow_legends.json"
+                }
         }
-    return render(request, "index.html", context=data)
+        for place in places
+        ]
+
+    context = {
+        "type": "FeatureCollection",
+        "features": features}
+
+    return render(request, "index.html", context=context)
