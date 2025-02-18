d2ee72aced9b5f85bbce1c3ab01b769f0c8ca89a
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-18 20:35:58 +0300

    Подключаю ответ

--- catalog/views.py
+++ catalog/views.py
@@ -1,12 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from catalog.models import Place, Image
+from django.http import JsonResponse, HttpResponse
+from django.urls import reverse
 
 # Create your views here.
 def index(request):
-    print("def index(request)")
     places = Place.objects.all()
-    print("places")
-    print(places)
     features = [
         {
             "type": "Feature",
@@ -17,19 +16,35 @@ def index(request):
             "properties": {
                 "title": place.title,
                 "placeId": place.id,
-                "detailsUrl": "static/places/moscow_legends.json"
+                "detailsUrl": reverse("place_detail", args=[place.id])
                 }
         }
         for place in places
         ]
-    print("features")
-    print(features)
     context = {
         "places": {
-        "type": "FeatureCollection",
-        "features": features}
-    }
+            "type": "FeatureCollection",
+            "features": features
+            }
+        }
     print("context")
     print(context)
 
     return render(request, "index.html", context=context)
+
+
+def place_detail(request, place_id):
+    place = get_object_or_404(Place, id=place_id)
+    place_images = place.images.all()
+    response = {
+        "title": place.title,
+        "imgs": [place_image.image.url for place_image in place_images],
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.lng,
+            "lat": place.lat
+        }
+    }
+
+    return JsonResponse(response, json_dumps_params={'ensure_ascii': False})
