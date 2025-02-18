812a0e77f375128640e0de979ff1a0d67ca56f80
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-18 20:57:14 +0300

    Подключаю базу

--- mysite/urls.py
+++ mysite/urls.py
@@ -8,6 +8,6 @@ from catalog.views import index, place_detail
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
-    path("places/<int:place_id>/", place_detail, name="place_detail")
+    path('places/<int:place_id>/', place_detail, name='place_detail')
     ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
          + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
