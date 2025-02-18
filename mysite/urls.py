d7c9ed04c12cbaef08cde32a237caf29bd11cf9b
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-02-18 20:57:28 +0300

    Убрал лишнее

--- mysite/urls.py
+++ mysite/urls.py
@@ -9,5 +9,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path("", index),
     path('places/<int:place_id>/', place_detail, name='place_detail')
-    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)\
-         + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
