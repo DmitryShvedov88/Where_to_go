3448a04f566856f532a3c16768ac0ebe3399fc32
DmitryShvedov88 <95371870+DmitryShvedov88@users.noreply.github.com>
2025-03-10 20:13:48 +0300

    Снова правлю STATIC_ROOT

--- mysite/settings.py
+++ mysite/settings.py
@@ -121,7 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = '/home/DmitryShvedov88/'
+STATIC_ROOT = '/home/DmitryShvedov88/Poster-on-the-map/static'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_URL = '/media/'
