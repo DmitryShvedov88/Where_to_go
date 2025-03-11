6ba6961d3c573bf890d84737fca3a7ba0161c9c2
Шведов Дмитрий <shvedovds@kamaz.ru>
2025-03-11 16:43:12 +0300

    Улучшение 14.1

--- catalog/models.py
+++ catalog/models.py
@@ -4,11 +4,29 @@ from tinymce.models import HTMLField
 
 # Create your models here.
 class Place(models.Model):
-    title = models.CharField(max_length=200, verbose_name="Место", blank=True, null=True)
-    short_description = models.CharField(max_length=250, verbose_name='Короткое описание', blank=True, null=True)
-    long_description = HTMLField(verbose_name='Длинное описание', blank=True, null=True)
-    latitude = models.FloatField(verbose_name="Широта")
-    longitude = models.FloatField(verbose_name="Долгота")
+    title = models.CharField(
+        max_length=200,
+        verbose_name="Место",
+        blank=True,
+        null=True
+        )
+    short_description = models.CharField(
+        max_length=250,
+        verbose_name='Короткое описание',
+        blank=True,
+        null=True
+        )
+    long_description = HTMLField(
+        verbose_name='Длинное описание',
+        blank=True,
+        null=True
+        )
+    latitude = models.FloatField(
+        verbose_name="Широта"
+        )
+    longitude = models.FloatField(
+        verbose_name="Долгота"
+        )
 
     class Meta:
         ordering = ['title']
@@ -18,9 +36,22 @@ class Place(models.Model):
 
 
 class Image(models.Model):
-    place = models.ForeignKey(Place, verbose_name="Картинка места", on_delete=models.CASCADE, related_name="images")
-    image = models.ImageField(verbose_name="КАРТИНКА", db_index=True, upload_to="images/")
-    number = models.IntegerField(verbose_name="ПОЗИЦИЯ", db_index=True, default=0)
+    place = models.ForeignKey(
+        Place,
+        verbose_name="Картинка места",
+        on_delete=models.CASCADE,
+        related_name="images"
+        )
+    image = models.ImageField(
+        verbose_name="КАРТИНКА",
+        db_index=True,
+        upload_to="images/"
+        )
+    number = models.IntegerField(
+        verbose_name="ПОЗИЦИЯ",
+        db_index=True,
+        default=0
+        )
 
     class Meta:
         ordering = ['number']
